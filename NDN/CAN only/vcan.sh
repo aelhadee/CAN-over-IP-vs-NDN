b5eca59bc65b903f7e2ca0c9ba7baa3aba56eef5
AE <87952618+aelhadee@users.noreply.github.com>
2022-07-02 17:02:27 -0400

    Update vcan.sh

--- NDN/CAN only/vcan.sh
+++ NDN/CAN only/vcan.sh
@@ -6,9 +6,9 @@ ip link add dev vcan2 type vcan
 
 ip link set vcan0 # mtu not specified since it's a standard can bus
 ip link set vcan1 mtu 72 # to support CAN FD payload
-ip link set vcan2 mtu 72
+ip link set vcan2 mtu 72 # optional
 
 ip link set dev vcan0 up
 ip link set dev vcan1 up
-ip link set dev vcan2 up
+ip link set dev vcan2 up #optional
 nfdc strategy set /trailer/ECU/CAN /localhost/nfd/strategy/multicast
