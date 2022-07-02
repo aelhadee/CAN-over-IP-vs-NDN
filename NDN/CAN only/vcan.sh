b9952eeba389c0dde63eac6172cd970753cd219a
aelhadee <87952618+aelhadee@users.noreply.github.com>
2022-07-02 17:00:07 -0400

    Update vcan.sh

--- NDN/CAN only/vcan.sh
+++ NDN/CAN only/vcan.sh
@@ -4,8 +4,8 @@ ip link add dev vcan0 type vcan
 ip link add dev vcan1 type vcan
 ip link add dev vcan2 type vcan
 
-ip link set vcan0 mtu 72
-ip link set vcan1 mtu 72
+ip link set vcan0 # mtu not specified since it's a standard can bus
+ip link set vcan1 mtu 72 # to support CAN FD payload
 ip link set vcan2 mtu 72
 
 ip link set dev vcan0 up
